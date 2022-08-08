9772f09fd726efc2210f927bcca6f6c8078f7f56
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-08-09 01:50:56 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+let deutscheGerichte = [
+    'Speckkuchen',
+    'Thüringer Rostbratwurst',
+    'Quarkkeulchen',
+    'Sauerbraten'
+];
+
+deutscheGerichte.unshift(
+    'Bremer Knipp',
+    'Pinkel mit Grünkohl',
+    'Linseneintopf')
+    //Aufgabe 1_8
+    deutscheGerichte.shift([0]);
+
+console.log(deutscheGerichte)
+
